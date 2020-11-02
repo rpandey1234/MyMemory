02885182847da7a78791f5a420b626e7f01a41e6
Rahul Pandey <rpandey1234@gmail.com>
2020-11-01 16:15:19 -0800

    Part 2: Setup RecyclerView to show 4 by 2 grid

--- app/src/main/java/com/rkpandey/mymemory/MainActivity.kt
+++ app/src/main/java/com/rkpandey/mymemory/MainActivity.kt
@@ -2,10 +2,25 @@ package com.rkpandey.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+  
+  private lateinit var rvBoard: RecyclerView
+  private lateinit var tvNumMoves: TextView
+  private lateinit var tvNumPairs: TextView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    rvBoard = findViewById(R.id.rvBoard)
+    tvNumMoves = findViewById(R.id.tvNumMoves)
+    tvNumPairs = findViewById(R.id.tvNumPairs)
+
+    rvBoard.adapter = MemoryBoardAdapter(this, 8)
+    rvBoard.setHasFixedSize(true)
+    rvBoard.layoutManager = GridLayoutManager(this, 2)
   }
 }
---
+++ app/src/main/java/com/rkpandey/mymemory/MemoryBoardAdapter.kt
@@ -0,0 +1,51 @@
+package com.rkpandey.mymemory
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
+
+class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+  RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
+
+  companion object {
+    private const val TAG = "MemoryBoardAdapter"
+    private const val MARGIN_SIZE = 10
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val cardWidth = parent.width / 2 - 2 * MARGIN_SIZE
+    val cardHeight = parent.height / 4 - 2 * MARGIN_SIZE
+    val cardSideLength = min(cardWidth, cardHeight)
+    val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+    val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+    layoutParams.width = cardSideLength
+    layoutParams.height = cardSideLength
+    layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+    // Set the appropriate width/height of the view
+    return ViewHolder(view)
+  }
+
+  override fun getItemCount() = numPieces
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.bind(position)
+  }
+
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+
+    fun bind(position: Int) {
+      imageButton.setImageResource(R.drawable.ic_launcher_background)
+      imageButton.setOnClickListener {
+        Log.i(TAG, "Clicked on position $position")
+      }
+    }
+
+  }
+}
