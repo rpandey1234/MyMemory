a06834ba5b1cee61b9c5bdc2815b9e51c12b64b0
Rahul Pandey <rpandey1234@gmail.com>
2020-11-01 19:50:59 -0800

    Part 9: Setup RecyclerView for creation flow + ImagePickerAdapter

--- app/src/main/java/com/rkpandey/mymemory/creation/CreateActivity.kt
+++ app/src/main/java/com/rkpandey/mymemory/creation/CreateActivity.kt
@@ -1,25 +1,45 @@
 package com.rkpandey.mymemory.creation
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.rkpandey.mymemory.R
 import com.rkpandey.mymemory.models.BoardSize
 import com.rkpandey.mymemory.utils.EXTRA_BOARD_SIZE
 
 class CreateActivity : AppCompatActivity() {
 
+  private lateinit var rvImagePicker: RecyclerView
+  private lateinit var etGameName: EditText
+  private lateinit var btnSave: Button
+
+  private lateinit var imagePickerAdapter: ImagePickerAdapter
   private lateinit var boardSize: BoardSize
+  private val chosenImageUris = mutableListOf<Uri>()
   private var numImagesRequired = -1
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_create)
 
+    rvImagePicker = findViewById(R.id.rvImagePicker)
+    etGameName = findViewById(R.id.etGameName)
+    btnSave = findViewById(R.id.btnSave)
+
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
     boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
     numImagesRequired = boardSize.getNumPairs()
     supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+
+    imagePickerAdapter = ImagePickerAdapter(this, chosenImageUris, boardSize)
+    rvImagePicker.adapter = imagePickerAdapter
+    rvImagePicker.setHasFixedSize(true)
+    rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
   }
 
   override fun onOptionsItemSelected(item: MenuItem): Boolean {
---
+++ app/src/main/java/com/rkpandey/mymemory/creation/ImagePickerAdapter.kt
@@ -0,0 +1,64 @@
+package com.rkpandey.mymemory.creation
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.rkpandey.mymemory.R
+import com.rkpandey.mymemory.models.BoardSize
+import kotlin.math.min
+
+class ImagePickerAdapter(
+  private val context: Context,
+  private val imageUris: List<Uri>,
+  private val boardSize: BoardSize
+) : RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+  companion object {
+    private const val TAG = "ImagePickerAdapter"
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    Log.i(TAG, "onCreateViewHolder")
+    val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
+    val cardWidth = parent.width / boardSize.getWidth()
+    val cardHeight = parent.height / boardSize.getHeight()
+    val cardSideLength = min(cardWidth, cardHeight)
+    Log.i(TAG, "onCreateViewHolder $cardSideLength, $cardWidth, $cardHeight")
+    val layoutParams = view.findViewById<ImageView>(R.id.ivCustomImage).layoutParams
+    layoutParams.width = cardSideLength
+    layoutParams.height = cardSideLength
+    return ViewHolder(view)
+  }
+
+  override fun getItemCount() = boardSize.getNumPairs()
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    if (position < imageUris.size) {
+      holder.bind(imageUris[position])
+    } else {
+      holder.bind()
+    }
+  }
+
+  inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val ivCustomImage = itemView.findViewById<ImageView>(R.id.ivCustomImage)
+
+    fun bind() {
+      ivCustomImage.setOnClickListener {
+        // Should launch intent for user to select photos
+      }
+    }
+
+    fun bind(uri: Uri) {
+      ivCustomImage.setImageURI(uri)
+      ivCustomImage.setOnClickListener(null)
+    }
+
+  }
+
+}
